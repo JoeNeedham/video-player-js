9955df839638987d23a68ea2e011596377e97169
joeneedum <joeneedum@gmail.com>
2022-05-02 19:05:45 -0400

    commit

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ const duration = document.querySelector('.time-duration');
 const fullscreenBtn = document.querySelector('.fullscreen');
 
 // Play & Pause ----------------------------------- //
+function showPlayIcon(){
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
+}
 
 function togglePlay() {
     if (video.paused) {
@@ -18,14 +22,38 @@ function togglePlay() {
         playBtn.setAttribute('title', 'Pause');
     } else {
         video.pause();
-        playBtn.classList.replace('fa-pause', 'fa-play');
-        playBtn.setAttribute('title', 'Play');
-
+        showPlayIcon();
     }
 }
 
+// On Video End, show play button icon
+video.addEventListener('ended', showPlayIcon);
+
 
 // Progress Bar ---------------------------------- //
+
+// Calculate display time format
+function displayTime(time) {
+    const minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time % 60);
+    seconds = seconds > 9 ? seconds : `0${seconds}`
+    return `${minutes}:${seconds}`;
+}
+
+// Update progress bar as video plays
+function updateProgress() {
+    progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`
+    currentTime.textContent = `${displayTime(video.currentTime)} /`;
+    duration.textContent = `${displayTime(video.duration)}`;
+}
+
+// Click to seek within the video
+function setProgress(e) {
+    const newTime = e.offsetX / progressRange.offsetWidth;
+    progressBar.style.width = `${newTime * 100}%`;
+    video.currentTime = newTime * video.duration;
+    console.log(newTime);
+}
 
 
 
@@ -42,3 +70,7 @@ function togglePlay() {
 // Event Listners
 playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
+progressRange.addEventListener('click', setProgress);
+
